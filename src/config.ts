7b56970faf82ae7fe055faf1e8a20b9facd95b19
github-personal <paf_uzair@yahoo.com>
2023-07-31 17:12:20 +0200

    chore: add src folder to the repo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Uzair Khan | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'Welcome to Uzair Khan\'s Personal Website! Explore the world of software quality assurance through the eyes of an experienced Test Automation Engineer proficient in Cypress, JavaScript, Cucumber, and more. Discover automation tips, industry insights, and successful projects. Connect with me and let\'s elevate software testing together! This website contains Blog, CV, Project Section and RSS Feed.';
